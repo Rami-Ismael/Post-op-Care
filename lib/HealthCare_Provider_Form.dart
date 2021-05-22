f52bc4fff68d7e39cfc672e485be9cf61156aa97
Rami <rami.m.ismael@gmail.com>
2021-05-21 20:08:54 -0500

    create a new class for a form

---
+++ lib/HealthCare_Provider_Form.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+class HealthCare_Provider_Form extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
